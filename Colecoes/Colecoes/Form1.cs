c92eecd9dfb59d2146be1fd251e8f13d68c37dc3
SamaraParcero <samaraparceroti@gmail.com>
2025-11-10 18:49:51 -0300

    Add Collection Learnings

---
+++ Colecoes/Colecoes/Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Colecoes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+
+            //Array
+            string[] nomes = new string[3];
+            nomes[0] = "Sas";
+            nomes[1] = "Samie";
+            nomes[2] = "Sami";
+
+            //Lista
+            List<string> nomes2 = new List<string>();
+            nomes2.Add("Samara");
+            nomes2.Add("Sam");
+            
+            //Adiciona uma coleção a lista
+            nomes2.AddRange(nomes);
+
+            //Remover elementos
+            /*if(nomes2.Remove("Sami"))
+            {
+                MessageBox.Show("Removido!");
+            }
+            else
+            {
+                MessageBox.Show("Não foi removido!");
+            }
+
+            //Lista contém o item?
+            string mensagem = nomes2.Contains("Danny") ? "Contém" : "Não contém!";
+            MessageBox.Show(mensagem);*/
+
+            //Contar elementos
+            //MessageBox.Show("Número de elementos: " + nomes2.Count);
+
+            //Organiza lista em ordem alfabética
+            nomes2.Sort();
+
+            //Saber indice de um elemento
+            //MessageBox.Show("Sam está no indice " + nomes2.IndexOf("Sam"));
+
+            //Adicionar elemento num indice específico
+            nomes2.Insert(2, "Pedro");
+
+            //Remover item de um indice
+            nomes2.RemoveAt(1);
+
+            //Limpa a lista toda
+            //nomes2.Clear();
+
+            foreach (string nome in nomes2)
+            {
+                lista.Items.Add(nome);
+            }
+     
+
+        }
+
+        private void btnHashSet_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+            //É uma lista que não aceita repetições
+            HashSet<string> veiculos = new HashSet<string>()
+            {
+                "Carro", "Moto", "Helicoptero", "Avião", "Barco"
+            };
+
+            //Para acessar o indice em HashSet
+            MessageBox.Show(veiculos.ElementAt(2));
+
+            /*if (veiculos.Add("Cavalo"))
+            {
+                MessageBox.Show("Item adicionado!");
+            } else
+            {
+                MessageBox.Show("Não adicionado!");
+            }
+
+            if (veiculos.Contains("Carro"))
+            {
+                MessageBox.Show("Contém");
+            }
+            else
+            {
+                MessageBox.Show("Não Contém");
+            }*/
+
+            //veiculos.Remove("Barco");
+            //int num = veiculos.Count;
+
+            //veiculos.Clear();
+
+            //Retorna primeiro elemento
+            //veiculos.First();
+
+            //Retorna último elemento
+            //veiculos.Last();
+
+            foreach (string veiculo in veiculos)
+            {
+                lista.Items.Add(veiculo);
+            }
+            
+        }
+
+        private void btnDictionary_Click(object sender, EventArgs e)
+        {
+            Dictionary<int, string> alunos = new Dictionary<int, string>()
+            {
+                {150, "Pedro" },
+                {200, "Lilian" },
+                {80, "Pedro" }
+            };
+
+            //Não pode repetir chave
+            alunos.Add(100, "Samara");
+
+
+            //Remove item
+            alunos.Remove(80);
+
+
+            //Quantidade de elementos
+            MessageBox.Show("Quantidade de alunos: " + alunos.Count);
+
+            //Limpa o dictionary
+            //alunos.Clear();
+
+            //Mostra primeiro item:
+            MessageBox.Show("Primeiro: " + alunos.First().Value);
+
+            KeyValuePair<int, string> primeiro = alunos.First();
+            MessageBox.Show("Primeiro: " + primeiro.Value);
+
+            //Mostra o ultimo
+            KeyValuePair<int, string> ultimo = alunos.Last();
+            MessageBox.Show("Ultimo: " + ultimo.Value);
+
+            //Vê Se contem essa chave
+            if (alunos.ContainsKey(80))
+            {
+                MessageBox.Show("Contém a chave 80");
+            }
+            else
+            {
+                MessageBox.Show("Não contém a chave 80");
+            }
+
+            //Vê Se contem esse valor
+            if (alunos.ContainsValue("Samara"))
+            {
+                MessageBox.Show("Contém o aluno");
+            }
+            else
+            {
+                MessageBox.Show("Não contém o aluno");
+            }
+
+
+            foreach (KeyValuePair<int, string> item in alunos)
+                {
+                    //Se eu quiser pegar somente a key
+                    lista.Items.Add(item.Key);
+                    //Se eu quiser pegar somente o valor
+                    lista.Items.Add(item.Value);
+
+                    lista.Items.Add(item.Key + " = " + item.Value);
+                }
+
+
+
+        }
+
+        private void btnSortedList_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+            //Uma dictionary que organiza os items baseado na key
+            SortedList<int, string> alunos = new SortedList<int, string>()
+            {
+                {27, "Danny" },
+                {10, "Gabriel" },
+                {17, "Arthir" }
+            };
+
+            alunos.Add(14, "Gloria");
+            //Apresenta na ordem reversa com o Reverse
+            //alunos.Reverse();
+
+            //Remove item da sortedList 
+            alunos.Remove(10);
+
+            //Remove baseado no indice conforme a ordem da lista
+            alunos.RemoveAt(0);
+
+            //alunos.Count;
+
+            //alunos.ContainsKey(50);
+
+            //alunos.ContainsValue("Danny");
+
+            foreach (KeyValuePair<int, string> item in alunos.Reverse())
+            {
+                lista.Items.Add(item.Key + " " + item.Value);  
+            }
+        }
+
+        private void btnSortedDictionary_Click(object sender, EventArgs e)
+        {
+            SortedDictionary<int, string> alunos = new SortedDictionary<int, string>
+            {
+                {20,"Samara" },
+                {300, "Wesley" },
+                {123, "Silvana" }
+            };
+
+            alunos.Add(5478, "Flavio");
+            alunos.Remove(20);
+            //alunos.Count;
+            //alunos.Clear();
+            MessageBox.Show(alunos.ElementAt(0).ToString());
+
+            alunos.ContainsKey(5478);
+            alunos.ContainsValue("Flavio");
+            foreach(KeyValuePair<int, string> item in alunos.Reverse())
+            {
+                lista.Items.Add(item);
+            }
+        }
+
+        private void btnSortedSet_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+            SortedSet<string> nomes = new SortedSet<string>()
+            {
+                "Samara",
+                "Parcero",
+                "Oliveira"
+            };
+
+            if (!nomes.Add("Samara"))
+            {
+                MessageBox.Show("Não pode repetir valor");
+            }
+
+            //nomes.Reverse();
+            nomes.Remove("Oliveira");
+            //MessageBox.Show(nomes.ElementAt(1));
+            MessageBox.Show(nomes.First());
+            MessageBox.Show(nomes.Last());
+            MessageBox.Show(nomes.Count.ToString());
+            //nomes.Clear();
+
+            foreach (string nome in nomes)
+            {
+                lista.Items.Add(nome);
+            }
+
+        }
+
+        private void btnQueue_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+            Queue<string> fila = new Queue<string>();
+
+            //Enfileirar
+            fila.Enqueue("Samara");
+            fila.Enqueue("Pedro");
+            fila.Enqueue("Lilian");
+
+            MessageBox.Show(fila.Count.ToString());
+
+            foreach(string nome in fila)
+            {
+                lista.Items.Add(nome);
+            }
+
+            //Retorna objeto no início da fila sem remover
+            MessageBox.Show("Primeiro da fila: "+ fila.Peek());
+
+            //Retira o primeiro objeto da fila
+            MessageBox.Show("Remover do início: " + fila.Dequeue());
+
+
+            //Retorna objeto no início da fila 
+            MessageBox.Show("Primeiro da fila: " + fila.First());
+
+            //Retorna objeto no fim da fila 
+            MessageBox.Show("Último da fila: " + fila.Last());
+
+            //fila.Clear();
+
+            while (fila.Count > 0)
+            {
+                //Retira o primeiro objeto da fila
+                MessageBox.Show("Remover do início: " + fila.Dequeue());
+                MessageBox.Show("Contagem: " + fila.Count.ToString());
+                lista.Items.Clear();
+                foreach (string nome in fila)
+                {
+                    lista.Items.Add(nome);
+                }
+            }
+
+        }
+
+        private void btnStack_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+            Stack<string> pilha = new Stack<string>();
+            pilha.Push("Samara");
+            pilha.Push("Pedro");
+            pilha.Push("Lilian");
+
+            MessageBox.Show(pilha.Count.ToString());
+
+            foreach(string nome in pilha)
+            {
+                lista.Items.Add(nome);  
+            }
+
+            MessageBox.Show("Elemento no topo da pilha: " + pilha.Peek());
+            MessageBox.Show(pilha.Count.ToString());
+
+            MessageBox.Show("Remover elemento do topo: " + pilha.Pop());
+            MessageBox.Show(pilha.Count.ToString());
+
+            while (pilha.Count > 0)
+            {
+                //Retira o topo da pilha
+                MessageBox.Show("Remover do início: " + pilha.Pop());
+                MessageBox.Show("Contagem: " + pilha.Count.ToString());
+                lista.Items.Clear();
+                foreach (string nome in pilha)
+                {
+                    lista.Items.Add(nome);
+                }
+            }
+
+        }
+    }
+}
---
+++ Jokenpo/Jokenpo/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Jokenpo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitulo = new System.Windows.Forms.Label();
+            this.pictureBoxJogador = new System.Windows.Forms.PictureBox();
+            this.pictureBoxPC = new System.Windows.Forms.PictureBox();
+            this.pictureBoxResultado = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.btnPedra = new System.Windows.Forms.Button();
+            this.btnPapel = new System.Windows.Forms.Button();
+            this.btnTesoura = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxJogador)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResultado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitulo.Location = new System.Drawing.Point(12, 9);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(849, 111);
+            this.labelTitulo.TabIndex = 0;
+            this.labelTitulo.Text = "Escolha: Pedra, Papel ou Tesoura..";
+            // 
+            // pictureBoxJogador
+            // 
+            this.pictureBoxJogador.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBoxJogador.Location = new System.Drawing.Point(50, 123);
+            this.pictureBoxJogador.Name = "pictureBoxJogador";
+            this.pictureBoxJogador.Size = new System.Drawing.Size(200, 200);
+            this.pictureBoxJogador.TabIndex = 1;
+            this.pictureBoxJogador.TabStop = false;
+            // 
+            // pictureBoxPC
+            // 
+            this.pictureBoxPC.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBoxPC.Location = new System.Drawing.Point(333, 123);
+            this.pictureBoxPC.Name = "pictureBoxPC";
+            this.pictureBoxPC.Size = new System.Drawing.Size(200, 200);
+            this.pictureBoxPC.TabIndex = 2;
+            this.pictureBoxPC.TabStop = false;
+            // 
+            // pictureBoxResultado
+            // 
+            this.pictureBoxResultado.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBoxResultado.Location = new System.Drawing.Point(621, 123);
+            this.pictureBoxResultado.Name = "pictureBoxResultado";
+            this.pictureBoxResultado.Size = new System.Drawing.Size(200, 200);
+            this.pictureBoxResultado.TabIndex = 3;
+            this.pictureBoxResultado.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(103, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 50);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Você";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(398, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 50);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PC";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(256, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 50);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "VS";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(566, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 50);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "=";
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResultado.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.labelResultado.Location = new System.Drawing.Point(701, 196);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(45, 50);
+            this.labelResultado.TabIndex = 8;
+            this.labelResultado.Text = "?";
+            // 
+            // btnPedra
+            // 
+            this.btnPedra.BackgroundImage = global::Jokenpo.Properties.Resources.Pedra;
+            this.btnPedra.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPedra.Location = new System.Drawing.Point(50, 340);
+            this.btnPedra.Name = "btnPedra";
+            this.btnPedra.Size = new System.Drawing.Size(200, 200);
+            this.btnPedra.TabIndex = 9;
+            this.btnPedra.UseVisualStyleBackColor = true;
+            this.btnPedra.Click += new System.EventHandler(this.btnPedra_Click);
+            // 
+            // btnPapel
+            // 
+            this.btnPapel.BackgroundImage = global::Jokenpo.Properties.Resources.Papel;
+            this.btnPapel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPapel.Location = new System.Drawing.Point(333, 340);
+            this.btnPapel.Name = "btnPapel";
+            this.btnPapel.Size = new System.Drawing.Size(200, 200);
+            this.btnPapel.TabIndex = 10;
+            this.btnPapel.UseVisualStyleBackColor = true;
+            this.btnPapel.Click += new System.EventHandler(this.btnPapel_Click);
+            // 
+            // btnTesoura
+            // 
+            this.btnTesoura.BackgroundImage = global::Jokenpo.Properties.Resources.Tesoura;
+            this.btnTesoura.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnTesoura.Location = new System.Drawing.Point(621, 340);
+            this.btnTesoura.Name = "btnTesoura";
+            this.btnTesoura.Size = new System.Drawing.Size(200, 200);
+            this.btnTesoura.TabIndex = 11;
+            this.btnTesoura.UseVisualStyleBackColor = true;
+            this.btnTesoura.Click += new System.EventHandler(this.btnTesoura_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 589);
+            this.Controls.Add(this.btnTesoura);
+            this.Controls.Add(this.btnPapel);
+            this.Controls.Add(this.btnPedra);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxResultado);
+            this.Controls.Add(this.pictureBoxPC);
+            this.Controls.Add(this.pictureBoxJogador);
+            this.Controls.Add(this.labelTitulo);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxJogador)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResultado)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitulo;
+        private System.Windows.Forms.PictureBox pictureBoxJogador;
+        private System.Windows.Forms.PictureBox pictureBoxPC;
+        private System.Windows.Forms.PictureBox pictureBoxResultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.Button btnPedra;
+        private System.Windows.Forms.Button btnPapel;
+        private System.Windows.Forms.Button btnTesoura;
+    }
+}
+
