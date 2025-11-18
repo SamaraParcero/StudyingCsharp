7e4de3969b387a162bae2965c92c26fae940df2b
SamaraParcero <samaraparceroti@gmail.com>
2025-11-18 19:02:55 -0300

    Add ASP .NET core RazorPage learning

---
+++ RazorPagesMovies/RazorPagesMovies/Models/SeedData.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesMovies.Data;
+
+namespace RazorPagesMovies.Models
+{
+    public class SeedData
+    {
+
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesMoviesContext(serviceProvider.GetRequiredService<DbContextOptions<RazorPagesMoviesContext>>()))
+            {
+                if (context == null || context.Movie == null)
+                {
+                    throw new ArgumentNullException("Null RazorPagesMovieContext");
+                }
+
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Senhor dos anéis",
+                        ReleaseDate = DateTime.Parse("1999-1-10"),
+                        Genre = "Fantasia",
+                        Price = 50.99M
+                    },
+                    new Movie
+                    {
+                        Title = "O hobbit",
+                        ReleaseDate = DateTime.Parse("1998-1-10"),
+                        Genre = "Fantasia",
+                        Price = 30
+                    }, 
+                    new Movie
+                    {
+                        Title = "As branquelas",
+                        ReleaseDate = DateTime.Parse("2000-1-10"),
+                        Genre = "Comédia",
+                        Price = 33.99M
+                    },
+                    new Movie
+                    {
+                        Title = "1989",
+                        ReleaseDate = DateTime.Parse("1989-1-10"),
+                        Genre = "Ficção científica",
+                        Price = 20.80M
+                    }
+                    );
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
---
+++ RazorPagesMovies/RazorPagesMovies/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorPagesMovies.Data;
+using RazorPagesMovies.Models;
+namespace RazorPagesMovies
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<RazorPagesMoviesContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("RazorPagesMoviesContext") ?? throw new InvalidOperationException("Connection string 'RazorPagesMoviesContext' not found.")));
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                SeedData.Initialize(services);
+            }
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            } //Mapeia as exce��es
+
+            app.UseHttpsRedirection(); //Direciona solicita��es http para https
+            app.UseStaticFiles();//Permite arquivos est�ticos sejam atendidos no nosso app Ex: css, js
+
+            app.UseRouting();//Adiciona rotas no middleware
+
+            app.UseAuthorization();//Autoriza usu�rio a acessar recursos seguros
+
+            app.MapRazorPages();//Configura roteamento de p�ginas razor
+
+            app.Run();//Executa
+        }
+    }
+}
